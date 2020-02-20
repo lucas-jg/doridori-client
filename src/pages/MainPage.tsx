0637f72e3f53f3719852a2e3880a0dd53d69fa5c
lucas <kjk727@me.com>
2020-02-20 16:01:20 +0900

    Add Markdown renderer

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,16 @@
 import * as React from 'react'
 import PageTemplate from '../components/base/PageTemplate'
+import { Link } from 'react-router-dom'
 
 interface MainPageProps {}
 
 const MainPage: React.FC<MainPageProps> = () => {
-    return <PageTemplate>Main</PageTemplate>
+    return (
+        <PageTemplate>
+            <div>Main</div>
+            <Link to='/post'>Go Post</Link>
+        </PageTemplate>
+    )
 }
 
 export default MainPage
--- src/pages/PostPage.tsx
+++ src/pages/PostPage.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react'
 import PageTemplate from '../components/base/PageTemplate'
 import { Helmet } from 'react-helmet-async'
+import ReactMarkdown from 'react-markdown'
 
 interface PostPageProps {}
 
 const PostPage: React.FC<PostPageProps> = () => {
+    const input =
+        '# This is a MARK_DOWN header1\n\n## This is a header2\n\n### This is a header3\n\n * And this is a paragraph'
     return (
         <PageTemplate>
             <Helmet>
                 <title>Dori Post</title>
             </Helmet>
-            Post
+            <ReactMarkdown source={input} />
         </PageTemplate>
     )
 }
